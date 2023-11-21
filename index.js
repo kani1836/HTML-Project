cb2691a1e7afc6d4899908aebf15ade2ca2b63bc
kani <kani20207@gmail.com>
2023-11-21 15:06:33 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+//variables
+
+var no1 = 10 ;
+var name = "Kani";
+
+
+var n1 = 10;
+var n2 = 20;
+
+
+//Styles of Variables
+
+//parialcase
+var MyName ;
+
+//Camelcase
+var myName ;
+
+//underscore
+var my_name;
+
+
+
+
+//arrays
+var animal = ["Tiger", "Lion", "Cheetah"];
+
+var no =[1,2,3,4,5];
+no [5] = "Hi";
+
+no.push("kani");
+
+//console
+
+//console.log("hi");
+
+//Loop
+
+// for( var i=10; i<15; i++){
+// console.log( i);
+// }
+
+
+//WhileLoop
+
+// var i=10;
+
+// while (i<18){
+//     console.log( i);
+//     i++;
+
+// }
+
+
+// if (1==10) {
+//     console.log(true);
+// }
+
+// else{
+//     console.log(false)
+// }
+
+
+//objects
+
+var person = {
+    fname :"Kani",
+    lname : "Kumar",
+    age : "29",
+    addres: {
+        city :"Chennai",
+        state : "Tamilnadu",
+        country : "India",
+    }
+
+}
+
+console.log(person.fname +""+ person.lname +""+ person.addres.city);
+
+
+// alert (no.length)
+
+// alert(no);
+// alert (animal);
+// alert (animal[2]);
