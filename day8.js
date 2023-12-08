44d5aadddd9571665ecd01613d3d072b3b8cf8cd
kani <kani20207@gmail.com>
2023-12-08 12:43:25 +0530

    Js learning

---
+++ day8.js
@@ -0,0 +1,64 @@
+//Primitive data types (Single Value Variable) -- String, Numbers, Boolean
+//Non-Primitive data type (Multiple Values Variable) -- Array, Object
+
+
+//let const
+
+let word = "This is a example of a string"
+// word="updated";
+console.log(word)
+
+
+//Array
+
+let a1 = [1,3,5,7, "Hi", true]
+
+a1[2]=10;
+a1[10]=25;
+console.log("Printing array here", a1)
+
+
+//Object
+
+let details = {
+    name: "Kanikumar",
+    course : "Fullstack"
+}
+
+details["branch"] = "Porur"
+
+console.log("Printing Objects here", details)
+
+
+//Operators
+//Arithmetic Logical Comparision Ternary
+
+//Arithmetic
+// +,-,/,%
+
+let output = 10+5
+console.log("addition (5+5)", 5+5)
+console.log("division (10/2)",10 / 2)
+// console.log(output)
+
+
+
+//Logical - The output will be only boolean
+// <, <=, >, >=, ==, !=
+
+console.log ("Logical condition (10==10)",10==10)
+console.log ("Logical condition (10>11)", 10>11)
+
+//Comparision -> and& or||
+
+//&& operator -->If any one condition is false the output will be false
+console.log ("AND (10==10  && 10>11)", 10==10  && 10>11)
+// || operator -->If any one condition is true the output will be true
+console.log ("OR (10==10  || 10>11)",10==10  || 10>11)
+
+//Ternary
+
+// (condition) ? if true : if false
+
+let result = (10 < 5) ? "Okay I can accept" : "Condition is false so I am printing this";
+console.log("Ternary (10 < 5)", result)
---
+++ day9.js
@@ -0,0 +1,153 @@
+//Loops
+//Type of loops 
+//if, if-else, nested-if
+//for, while, for-of, for-in, do-while
+
+
+// Conditional loops - Writing functios based on conditions (True or False)
+
+
+//if and nested if and else
+console.log("if and nested if and else")
+let num = 16;
+let result = 0;
+
+if(num > 16){
+    result = result + 5;
+    console.log(result);
+    console.log("condition passed")
+}
+else if (num > 15){
+    console.log("condition 15")
+}
+else if (num > 12){
+    console.log("condition 17")
+}
+
+
+else{
+    console.log("failed")
+
+}
+
+//for loop
+console.log("for loop started");
+let arr = [4,7,8,6,2,3,4];
+ for (let i = 0; i < arr.length ; i++){
+    console.log(arr[i]);
+ }
+
+ //while loop
+
+ console.log("while loop started");
+
+ let y=0;
+ while(y<arr.length){
+    console.log(arr[y])
+    y++;
+ }
+
+  //for of
+
+  console.log("for of started");
+
+  for (let obj of arr){
+    console.log(obj)
+  }
+
+//for in
+console.log("for in started");
+
+let obj = {
+    name : "Kani",
+    course: "fullstack"
+}
+
+for (let ob in obj){
+  console.log(ob)
+  console.log(obj[ob])
+}
+
+
+//Switch Case
+
+console.log("Switch Case")
+
+switch("tuesday"){
+    case ("monday"):
+        console.log("Yes it is Monday")
+        break ;
+    case ("tuesday"):
+        console.log("Yes it is Tuesday")
+        break ;
+    case ("wednesday"):
+        console.log("Yes it is Wednesday")
+        break ;
+    default:
+        console.log("No option")
+        break;
+}
+
+let num5 = 5;
+let num6 = 6;
+let res1 = 0;
+
+switch("sub"){
+    case ("add"):
+      console.log(num5+num6) 
+        break ;
+    case ("sub"):
+        console.log(num5-num6) 
+        break ;
+    default:
+        console.log("No option")
+        break;
+}
+
+
+// String methods or functions - User defined methods and Inbuild methods
+
+//String inbuild methods
+
+let w1 = "This is a javascript class"
+// console.log(w1)
+// console.log(w1.toUpperCase())
+// //assigning
+// // w1 = w1.toUpperCase();
+// console.log(w1)
+console.log("Upper case method")
+console.log(w1.toUpperCase())
+
+console.log("Lowercase method")
+console.log(w1.toLowerCase())
+
+console.log("Split method")
+console.log(w1.split("a"))
+
+console.log("CharAt Method")
+console.log(w1.charAt(10))
+
+console.log("length Method")
+console.log(w1.length)
+
+console.log("merge method")
+console.log(w1.concat(" merge"))
+
+console.log("slice method")
+console.log(w1.slice(-5, -1)) //it will allow negative value
+
+console.log("substring method")
+console.log(w1.substring(10, 14))//it will not allow negative value but both are for same purpose
+
+console.log("substr method")
+console.log(w1.substr(10, 1))
+
+
+//test
+
+let test1 = "kanikumar";
+for(i = 0; i < test1.length; i++) {
+    if(i % 2 == 0) {
+        console.log(test1.charAt(i));
+    }
+}
